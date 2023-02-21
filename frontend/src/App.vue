1613c1b76ab7489ced0b52dbcf51918b84d1d2ba
thanhpv958 <thanhpv958@gmail.com>
2023-02-21 15:47:15 +0700

    init frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <th scope="row">2</th>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <th scope="row">3</th>
+          <td>Larry</td>
+          <td>the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+
+  data() {
+    return {
+      data: [],
+    };
+  },
+
+  async created() {
+    // GET request using fetch with async/await
+    const response = await fetch("https://api.npms.io/v2/search?q=vue");
+    const data = await response.json();
+    this.data = data.results;
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+table {
+  margin: 0px auto;
+}
+</style>
